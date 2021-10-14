b75e31c145c7df03d085268670caacba73650694
lexsideris <lexsideris@gmail.com>
2021-10-14 14:18:07 +0300

    Added app token

--- src/components/GoodLuck/GoodLuck.js
+++ src/components/GoodLuck/GoodLuck.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { useParams } from "react-router";
 
@@ -6,7 +5,7 @@ export default function GoodLuck() {
   let { language } = useParams();
 
   language = language || "bg";
-  window.location.href = `https://qphotography.eu/${language}/services/good-luck`;
+  window.location.href = `https://qphotography.eu/${language}/services/good-luck?t=p1ckd1yl7L5ck4m3`;
 
   return <h1></h1>;
 }
